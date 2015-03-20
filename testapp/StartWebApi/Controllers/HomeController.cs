ffacde17708bb94629f937e0f6d927c89e638bf4
Troy Dai <troy.dai@outlook.com>
2015-03-20 08:54:25 -0700

    Update applications: react to Hosting changes

--- testapp/StartWebApi/Controllers/HomeController.cs
+++ testapp/StartWebApi/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNet.Mvc;
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNet.Mvc;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
--- testapp/StartWebApi/Startup.cs
+++ testapp/StartWebApi/Startup.cs
@@ -1,23 +1,22 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
 using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Http;
-using Microsoft.AspNet.Routing;
 using Microsoft.Framework.DependencyInjection;
 
 namespace StartWebApi
 {
     public class Startup
     {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
         // Configure is called after ConfigureServices is called.
         public void Configure(IApplicationBuilder app)
         {
             app.UseStaticFiles();
-
-            app.UseServices(services =>
-            {
-                services.AddMvc();
-            });
 
             app.UseMvc(routes =>
             {
