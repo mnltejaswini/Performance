7157709f3c9730fe0787341a511f7a37e25ee9a4
Troy Dai <troy.dai@outlook.com>
2015-03-31 23:01:03 -0700

    Add BasicKestrel test app
    Not adding Kestrel to BasicWeb so as to keep BasicWeb startup time to minimal

---
+++ testapp/BasicKestrel/Startup.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Builder;
+
+namespace Microsoft.AspNet.Test.Perf.WebFx.Apps.HelloWorld
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                context.Response.ContentType = "text/plain";
+                await context.Response.WriteAsync("Hello world");
+            });
+        }
+    }
+}
