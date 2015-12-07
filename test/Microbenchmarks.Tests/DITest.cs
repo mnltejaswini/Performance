0413271d88e710754b0d35a1f1c596592c04de5a
N. Taylor Mullen <nimullen@microsoft.com>
2015-12-07 11:16:27 -0800

    Add DI `BuildServiceProvider` test.
    https://github.com/aspnet/Coherence-Signed/issues/123

---
+++ test/Microbenchmarks.Tests/DITest.cs
@@ -0,0 +1,72 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Benchmarks.Framework;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microbenchmarks.Tests
+{
+    public class DITest : BenchmarkTestBase
+    {
+        [Benchmark]
+        public void BuildServiceProvider()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddScoped<DependOnNonexistentService>();
+            serviceCollection.AddTransient<ClassDependsOnPrivateConstructorClass>();
+            serviceCollection.AddTransient<AbstractClass, ImplementedAbstractClass>();
+            serviceCollection.AddTransient<EmptyInterface, ImplementedEmptyInterface>();
+            serviceCollection.AddSingleton<ClassWithPrivateCtor>();
+
+            using (Collector.StartCollection())
+            {
+                serviceCollection.BuildServiceProvider();
+            }
+        }
+
+        private interface IFakeService
+        {
+        }
+
+        private class DependOnNonexistentService
+        {
+            public DependOnNonexistentService(IFakeService nonExistentService)
+            {
+            }
+        }
+
+        private class ClassDependsOnPrivateConstructorClass
+        {
+            public ClassDependsOnPrivateConstructorClass(ClassWithPrivateCtor value)
+            {
+
+            }
+        }
+
+        private abstract class AbstractClass
+        {
+            public AbstractClass()
+            {
+            }
+        }
+
+        private class ImplementedAbstractClass : AbstractClass
+        {
+        }
+
+        private class ClassWithPrivateCtor
+        {
+            private ClassWithPrivateCtor()
+            {
+            }
+        }
+
+        private interface EmptyInterface
+        {
+        }
+
+        private class ImplementedEmptyInterface : EmptyInterface
+        {
+        }
+    }
+}
