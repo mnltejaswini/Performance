ea333d2807be68822d9cc12d69b8e28fee9d39be
N. Taylor Mullen <nimullen@microsoft.com>
2015-12-07 16:50:49 -0800

    Added Mvc Routing tests.
    - Added test to go from route hit => MVC action invocation in memory.
    
    https://github.com/aspnet/Coherence-Signed/issues/123

---
+++ test/Microbenchmarks.Tests/Mvc/MvcRoutingTest.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Threading.Tasks;
+using Benchmarks.Framework;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microbenchmarks.Tests.Mvc
+{
+    public class MvcRoutingTest : BenchmarkTestBase
+    {
+        [Benchmark]
+        public async Task RouteToAction()
+        {
+            using (var testServer = TestServer.Create(
+                configureApp: app => app.UseMvcWithDefaultRoute(),
+                configureServices: services => services.AddMvc()))
+            {
+                using (var client = testServer.CreateClient())
+                {
+                    client.BaseAddress = new Uri("http://localhost");
+
+                    using (Collector.StartCollection())
+                    {
+                        var result = await client.GetAsync("Home");
+                        var stringResponse = await result.Content.ReadAsStringAsync();
+                    }
+                }
+            }
+        }
+    }
+}
