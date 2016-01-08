a55f56ea7538a5aef01c2e6c986e01e96fd48624
N. Taylor Mullen <nimullen@microsoft.com>
2016-01-07 16:26:30 -0800

    Add Razor parsing test to microbenchmarks test.

--- test/Microbenchmarks.Tests/RazorTests.cs
+++ test/Microbenchmarks.Tests/RazorTests.cs
@@ -1,11 +1,19 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using Benchmarks.Framework;
+using Microsoft.AspNet.FileProviders;
+using Microsoft.AspNet.Mvc.Razor;
+using Microsoft.AspNet.Mvc.Razor.Directives;
 using Microsoft.AspNet.Razor;
+using Microsoft.AspNet.Razor.CodeGenerators;
 using Microsoft.AspNet.Razor.Compilation.TagHelpers;
 using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+using Microsoft.Extensions.Primitives;
 using Xunit;
 
 namespace Microbenchmarks.Tests.Razor
@@ -37,6 +45,121 @@ namespace Microbenchmarks.Tests.Razor
 
             Assert.NotEmpty(descriptors);
             Assert.Empty(errorSink.Errors);
+        }
+
+        [Benchmark]
+        [BenchmarkVariation("Runtime", false)]
+        [BenchmarkVariation("Design Time", true)]
+        public void ViewParsing(bool designTime)
+        {
+            // Arrange
+            var chunkTreeCache = new DefaultChunkTreeCache(new TestFileProvider());
+            var razorHost = new MvcRazorHost(chunkTreeCache)
+            {
+                DesignTimeMode = designTime
+            };
+            var assembly = typeof(RazorTests).GetTypeInfo().Assembly;
+            var assemblyName = assembly.GetName().Name;
+            var stream = assembly.GetManifestResourceStream($"{assemblyName}.compiler.resources.RazorTests.TestFile.cshtml");
+            GeneratorResults result;
+
+            // Act
+            using (Collector.StartCollection())
+            {
+                result = razorHost.GenerateCode("test/path", stream);
+            }
+
+            // Assert
+            Assert.Empty(result.ErrorSink.Errors);
+            Assert.Empty(result.ParserErrors);
+            Assert.True(result.Success);
+        }
+
+        private class TestFileProvider : IFileProvider
+        {
+            public virtual IDirectoryContents GetDirectoryContents(string subpath)
+            {
+                throw new NotSupportedException();
+            }
+
+            public virtual IFileInfo GetFileInfo(string subpath)
+            {
+                return new NotFoundFileInfo();
+            }
+
+            public virtual IChangeToken Watch(string filter)
+            {
+                return new TestFileChangeToken();
+            }
+
+            private class NotFoundFileInfo : IFileInfo
+            {
+                public bool Exists => false;
+
+                public bool IsDirectory
+                {
+                    get
+                    {
+                        throw new NotImplementedException();
+                    }
+                }
+
+                public DateTimeOffset LastModified
+                {
+                    get
+                    {
+                        throw new NotImplementedException();
+                    }
+                }
+
+                public long Length
+                {
+                    get
+                    {
+                        throw new NotImplementedException();
+                    }
+                }
+
+                public string Name
+                {
+                    get
+                    {
+                        throw new NotImplementedException();
+                    }
+                }
+
+                public string PhysicalPath
+                {
+                    get
+                    {
+                        throw new NotImplementedException();
+                    }
+                }
+
+                public Stream CreateReadStream()
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+            private class TestFileChangeToken : IChangeToken
+            {
+                public bool ActiveChangeCallbacks => false;
+
+                public bool HasChanged { get; set; }
+
+                public IDisposable RegisterChangeCallback(Action<object> callback, object state)
+                {
+                    return new NullDisposable();
+                }
+
+                private class NullDisposable : IDisposable
+                {
+                    public void Dispose()
+                    {
+                    }
+                }
+            }
         }
     }
 }
---
+++ test/Microbenchmarks.Tests/TestModels/User.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microbenchmarks.Tests.TestModels
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Blurb { get; set; }
+
+        public DateTimeOffset DateOfBirth { get; set; }
+
+        public int YearsEmployeed { get; set; }
+    }
+}
