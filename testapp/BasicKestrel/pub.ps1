28438af7ae050ea9c7f56c09828936e29639b4e8
Troy Dai <troy.dai@outlook.com>
2016-01-08 14:36:50 -0800

    update

--- testapp/BasicKestrel/pub.ps1
+++ testapp/BasicKestrel/pub.ps1
@@ -1,5 +1,6 @@
 dotnet restore
-dotnet publish -o .\publish\dotnet --framework dnxcore50
+dotnet publish -o .\publish\dotnet --framework dnxcore50 --configuration release
 cp .\hosting.json .\publish\dotnet\ -force
 
-dnu publish -o .\publish\dnx --no-source --runtime active
+dnu publish -o .\publish\dnx --no-source --framework dnxcore50 --configuration release
+
--- testapp/BasicKestrel/test.ps1
+++ testapp/BasicKestrel/test.ps1
@@ -1,29 +1,43 @@
+function start_dotnet_web()
+{
+    start .\publish\dotnet\web.exe
+}
+
+function stop_dotnet_web()
+{
+    ps web | kill
+}
+
+function start_dnx_web()
+{
+    start .\publish\dnx\approot\web.cmd
+}
+
+function stop_dnx_web()
+{
+    ps dnx | kill
+} 
+
+function perf_test()
+{
+    $begin = [System.DateTime]::Now
+    invoke-webrequest http://localhost:5000 | out-null
+    $end = [System.DateTime]::Now
+
+    Write-Host "Startup in: " + ($end - $begin).TotalMilliseconds
+
+    c:\tools\ab.exe -n5000 -c16 -q -d http://127.0.0.1:5000/
+}
+
 # dotnet based Kestrel
 
-$begin = [System.DateTime]::Now
-
-start .\publish\dotnet\web.exe
-invoke-webrequest http://localhost:5000 | out-null
-
-$end = [System.DateTime]::Now
-
-Write-Host "Startup in: " + ($end - $begin).TotalMilliseconds
-
-c:\tools\ab.exe -n5000 -c16 -q -d http://127.0.0.1:5000/
-
-ps web | kill
+start_dotnet_web
+perf_test
+stop_dotnet_web
 
 # dnx based Kestrel
 
-$begin = [System.DateTime]::Now
+start_dnx_web
+perf_test
+stop_dnx_web
 
-start .\publish\dnx\approot\web.cmd
-invoke-webrequest http://localhost:5000 | out-null
-
-$end = [System.DateTime]::Now
-
-Write-Host "Startup in: " + ($end - $begin).TotalMilliseconds
-
-c:\tools\ab.exe -n5000 -c16 -q -d http://127.0.0.1:5000/
-
-ps dnx | kill
